3dfd63f8c6bb6b71a62713c6e66c9bb3860e491e
dhruvpatt <84606623+dhruvpatt@users.noreply.github.com>
2022-07-07 10:43:47 -0400

    Update counter.js

--- projects/course-info/src/counter.js
+++ projects/course-info/src/counter.js
@@ -3,13 +3,29 @@ import { useState } from "react";
 
 const App = () =>{
 
-    const [counter, setCounter] = useState(0);
-
-    setTimeout( () => setCounter(counter + 1), 1000)
-
-
+    const [counter, setCounter] = useState(0)
+    
+    const increaseByOne = () => {
+        setCounter(counter + 1)
+    }
+    const setToZero = () => {
+        setCounter(0)
+    }
+    // setTimeout( () => setCounter(counter + 1), 1000)
     return(
-        <div>{counter}</div>
+        <div>
+            <Display counter={counter} />
+            <button onClick={increaseByOne}>Add One</button>
+            <button onClick={setToZero}>Set to Zero</button>
+        </div>
     )
+}
+
+const Display = (props) =>{
+    return(
+        <div>{props.counter}</div>
+    )
+
+
 }
 export default App;
